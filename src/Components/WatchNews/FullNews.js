59a56384dfa9000c7bc8fae3873b495f027f5e39
Alexandre Eugene <you@example.com>
2022-09-13 15:16:07 +0200

    appel api

---
+++ src/Components/WatchNews/FullNews.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
+
+
+const FullNews = () => {
+    const [articles, setArticles] = useState([])
+
+
+
+    const getArticles = () => {
+        return fetch("https://newsapi.org/v2/everything?q=apple&from=2022-09-12&to=2022-09-12&sortBy=popularity&apiKey=ade5773e72964d6a9b4889ad02a92802", {
+            type: "GET",
+        }).then((res) => res.json())
+            .then(data => setArticles(data.articles))
+
+
+    };
+    useEffect(() => {
+        getArticles()
+
+    }, [])
+    console.log(articles);
+    return (
+
+        <ul>
+            {
+                articles.map((article) =>
+                    <div key={uuidv4()}>
+
+                        <li>{article.publishedAt} </li>
+                        <li>{article.description} </li>
+                        <li>{article.content} </li>
+                        <li>{article.source.name} </li>
+                        <li>{article.author} </li>
+                        <li>{article.title} </li>
+                        <li><a href={article.url}> {article.url}</a> </li>
+                        <li><img src={article.urlToImage} style={{ width: 400 }} alt="" srcset="" />{article.title} </li>
+
+                    </div>
+                )
+            }
+
+        </ul >
+    );
+};
+export default FullNews;
