a55237f43157c9c273a3d0b60f65bcf6e0291b7e
Alexandre Eugene <you@example.com>
2022-09-16 15:40:01 +0200

    recherche par langue

--- src/Components/WatchNews/FullNews.js
+++ src/Components/WatchNews/FullNews.js
@@ -8,29 +8,48 @@ import { SearchBar } from '..';
 const FullNews = () => {
     const [articles, setArticles] = useState([])
     const [allArticles, setAllArticles] = useState(true)
+    const options = [
+        { value: 'fr', label: 'Français' },
+        { value: 'de', label: 'Allemand' },
+        { value: 'ar', label: 'Arabe' },
+        { value: 'zh', label: 'Chinois' },
+        { value: 'es', label: 'Espagnol' },
+        { value: 'it', label: 'Italien' },
+        { value: 'pt', label: 'Portugais' },
+        { value: 'ru', label: 'Russe' },
+        { value: 'sv', label: 'Suèdois' },
+    ]
+    const [language, setLanguage] = useState('fr')
+    const handleChange = (e) => {
+        setLanguage(e.value)
+    }
 
     useEffect(() => {
         console.log("allArticles", allArticles);
         if (allArticles) {
-            fetch("https://newsapi.org/v2/everything?q=all&apiKey=ade5773e72964d6a9b4889ad02a92802", {
+            fetch(`https://newsapi.org/v2/everything?q=all&language=${language}&apiKey=ade5773e72964d6a9b4889ad02a92802`, {
                 type: "GET",
             }).then((res) => res.json())
                 .then(data => setArticles(data.articles))
 
         } else {
-            fetch("https://newsapi.org/v2/top-headlines?country=fr&apiKey=ade5773e72964d6a9b4889ad02a92802", {
+            fetch(`https://newsapi.org/v2/top-headlines?language=${language}&apiKey=ade5773e72964d6a9b4889ad02a92802`, {
                 type: "GET",
             }).then((res) => res.json())
                 .then(data => setArticles(data.articles))
 
         }
-    }, [allArticles])
+    }, [allArticles, language])
 
     return (
         <div>
             <SearchBar
                 setAllArticles={setAllArticles}
                 allArticles={allArticles}
+                options={options}
+                language={language}
+                setLanguage={setLanguage}
+                handleChange={handleChange}
             />
             <div className="new-container">
 
--- src/Components/WatchNews/SearchBar.js
+++ src/Components/WatchNews/SearchBar.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Select from 'react-select'
 import toggleArticle from '../../functional/toggle'
 
-export default function searchBar({ setAllArticles, allArticles }) {
+export default function SearchBar({ setAllArticles, allArticles, options, language, setLanguage, handleChange }) {
 
+    console.log(language);
     return (
-        <button onClick={() => toggleArticle(setAllArticles, allArticles)}>tout les articles</button>
+        <div className='search-container'>
+            <button className='button-allArticles' onClick={() => toggleArticle(setAllArticles, allArticles)}>{allArticles ? "les plus cherché" : "tout les articles"}</button>
+            <Select className='select-searchBar' options={options} onChange={handleChange} value={language} placeholder='Language' />
+        </div>
     )
 }
